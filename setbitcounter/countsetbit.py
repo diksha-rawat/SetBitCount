4734d59a762183f5fd6aa2b6f444b40d7e2fa0f3
diksha-rawat <42347815+diksha-rawat@users.noreply.github.com>
2019-02-03 10:54:52 +0530

    Update countsetbit.py

--- setbitcounter/countsetbit.py
+++ setbitcounter/countsetbit.py
@@ -1,15 +1,13 @@
-def countset(start,end=0):
+def countsetbit(start,end=0):
     count=0
     if end==0:
         c=bin(start)
         binary=c[2:]
-        if '1' in binary:
-            count+=int(binary.count('1'))
+        count+=int(binary.count('1'))
     else:
         for i in range(start,end+1):
             c=bin(i)
             binary=c[2:]
-            if '1' in binary:
-                count+=int(binary.count('1'))       
+            count+=int(binary.count('1'))       
     return(count)
 
